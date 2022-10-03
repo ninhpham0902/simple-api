5d53033b90b53d9eb9a2cc351d1ec9500557cbfa
Ninh Pham <70870570+ninhpham0902@users.noreply.github.com>
2022-10-03 21:29:22 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
-api = Api(app)
 
 parser = reqparse.RequestParser()
 class TimezoneList(Resource):
